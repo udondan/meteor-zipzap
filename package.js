3dbb1316fea0729b0c84caf0e0a33c0d853a45b0
udondan <deemes79@gmail.com>
2014-05-17 16:33:33 +0200

    Added Blob.js for Blob() support in old browsers

--- package.js
+++ package.js
@@ -1,9 +1,10 @@
 Package.describe({
-  summary: "A Zip package for Meteor",
+  summary: "Zip and unzip files on server and clientside",
   environments: ['server', 'client']
 });
 
 Package.on_use(function (api) {
+  api.add_files('vendor/blob/Blob.js', 'client');
   api.add_files('vendor/jszip/dist/jszip.js', ['client', 'server']);
   api.add_files('lib/saveAs.js', 'client');
   api.add_files('vendor/filesaver/FileSaver.js', 'client');
